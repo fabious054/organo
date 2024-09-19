548ca872218799af65c50b9e94ffd718e84fa2a7
Fabio <fabio.h.abreu99@gmail.com>
2024-09-19 19:00:29 -0300

    option to change de backgroud squad color

--- src/components/Squad/index.js
+++ src/components/Squad/index.js
@@ -1,10 +1,15 @@
 import Member from '../Member';
+import { useState } from 'react';
 import './Squad.css';
 
 const Squad = ({name, members,principalColor,secondaryColor,deleteMember}) => {
+
+    const [colorBg, setColorBg] = useState(secondaryColor);
+
     return (
         members.length > 0 ? 
-            <section style={{backgroundImage:'url(/images/fundo.png)',backgroundColor:secondaryColor}} className='squad'>
+            <section style={{backgroundImage:'url(/images/fundo.png)',backgroundColor:colorBg}} className='squad'>
+                <input type='color' onChange={(color) => setColorBg(color.target.value)} value={principalColor} className='color-input' />
                 <h3 style={{borderBottom:`4px solid ${principalColor}`}}>{name}</h3>
                 <div className='members'>
                     {members.map((member, index) => (
