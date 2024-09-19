209a651f4078ece552736bd202e9373588eca451
Fabio <fabio.h.abreu99@gmail.com>
2024-09-19 15:44:52 -0300

    member component

---
+++ src/components/Member/index.js
@@ -0,0 +1,18 @@
+import './Member.css';
+
+const Member = () => {
+    return (
+        <div className='member'>
+            <div className='header'>
+                <img src='https://github.com/fabious054.png'alt='Fabio Abreu'/>
+            </div>
+            <div className='footer'>
+                <h4>Fabio Abreu</h4>
+                <h5>DEV</h5>
+            </div>
+        </div>
+    );
+
+}
+
+export default Member;
--- src/components/Squad/index.js
+++ src/components/Squad/index.js
@@ -1,9 +1,11 @@
+import Member from '../Member';
 import './Squad.css';
 
 const Squad = ({name, members,principalColor,secondaryColor}) => {
     return (
         <section style={{backgroundColor:secondaryColor}} className='squad'>
             <h3 style={{borderBottom:`4px solid ${principalColor}`}}>{name}</h3>
+            <Member />
         </section>
     );
 }
